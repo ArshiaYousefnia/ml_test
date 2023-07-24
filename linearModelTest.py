1411f822ba6047576824022721a687686852a386
Arshia <arshiayousefnia@yahoo.com>
2023-07-25 01:25:07 +0330

    classification problem with logistic regression

--- linearModelTest.py
+++ linearModelTest.py
@@ -5,11 +5,11 @@ from torch.autograd import Variable
 from linearRegression import LinearRegression
 import matplotlib.pyplot as plt
 
-x_values = [[1, 2], [1.5, 2.5], [2, 3], [5, 6], [8, 9], [5, 5], [6, 9], [1, 1]]
+x_values = [[1, 2], [1.5, 2.5], [2, 3], [5, 6], [8, 9], [5, 5], [6, 9], [1, 1], [1, 4], [1, 8], [1.5, 6], [4, 2], [3, 4], [6, 1], [8, 2], [4, 6], [4, 8], [4, 9], [3, 9], [6.5, 4], [6, 6], [8, 6], [9, 1], [9, 4], [5, 7], [7, 7]]
 x_train = np.array(x_values, dtype=np.float32)
 x_train = x_train.reshape(-1, 2)
 
-y_values = [1, 1, 1, 0, 0, 0, 0, 1]
+y_values = [1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 #y_values = [2 * i + 1 for i in x_values]
 y_train = np.array(y_values, dtype=np.float32)
 y_train = y_train.reshape(-1, 1)
@@ -17,10 +17,10 @@ y_train = y_train.reshape(-1, 1)
 inputDim = 2
 outputDim = 1
 learningRate = 0.01
-epochs = 2
+epochs = 100
 
 model = LinearRegression(inputDim, outputDim)
-criterion = torch.nn.CrossEntropyLoss()
+criterion = torch.nn.BCELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learningRate)
 
 losses = []
@@ -47,23 +47,18 @@ with torch.no_grad():
     border = model(Variable(torch.from_numpy(test_data))).data.numpy()
     print(border)
 
-
-#plt.clf()
-#plt.xlabel('epoch')
-#plt.ylabel('loss')
-#plt.plot(range(epochs), losses, label='loss', alpha=0.9)
-#plt.legend(loc='best')
-#plt.show()
-heat_map_data = np.zeros((100, 100))
-for i in range(100):
-    for j in range(100):
-        current = np.array([i / 10, j / 10], dtype=np.float32)
+heat_map_resolution = 200
+heat_map_data = np.zeros((heat_map_resolution, heat_map_resolution))
+for i in range(heat_map_resolution):
+    for j in range(heat_map_resolution):
+        current = np.array([i * 10 / heat_map_resolution, j * 10 / heat_map_resolution], dtype=np.float32)
         heat_map_data[i, j] = model(torch.from_numpy(current)).data.numpy()[0]
 
 plt.clf()
-plt.plot(x_train[:, 0] * 10, x_train[:, 1] * 10, '.', label='data', alpha=0.5)
-#plt.plot(test, label='test', alpha=0.5)
-plt.imshow(heat_map_data, cmap='autumn')
+plt.plot(x_train[:, 0] * heat_map_resolution / 10, x_train[:, 1] * heat_map_resolution / 10, '.')#, label='data', alpha=0.5)
+plt.ylim(0, heat_map_resolution)
+plt.imshow(heat_map_data, cmap='BuGn')
+plt.colorbar()
 plt.show()
 
 plt.clf()
--- linearRegression.py
+++ linearRegression.py
@@ -6,6 +6,7 @@ class LinearRegression(nn.Module):
     def __init__(self, input_size, output_size):
         super(LinearRegression, self).__init__()
         self.linear = torch.nn.Linear(input_size, output_size)
+        self.linear.zero_grad()
 
     def forward(self, x):
         out = torch.sigmoid(self.linear(x))
