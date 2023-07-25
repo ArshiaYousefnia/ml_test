2c256b4303409f5584d8d2d60c5d5d7ca6762502
Arshia <arshiayousefnia@yahoo.com>
2023-07-25 17:56:52 +0330

    improved multilayer perceptron for classification problem

--- MultiLayerPerceptron.py
+++ MultiLayerPerceptron.py
@@ -5,8 +5,8 @@ from torch import nn
 class MultiLayerPerceptron(nn.Module):
     def __init__(self, input_size):
         super(MultiLayerPerceptron, self).__init__()
-        self.linear1 = nn.Linear(input_size, 7)
-        self.linear2 = nn.Linear(7, 4)
+        self.linear1 = nn.Linear(input_size, 5)
+        self.linear2 = nn.Linear(5, 4)
         self.linear3 = nn.Linear(4, 1)
 
         self.linear1.zero_grad()
--- mlp_test.py
+++ mlp_test.py
@@ -4,18 +4,31 @@ from torch.autograd import Variable
 from MultiLayerPerceptron import MultiLayerPerceptron
 import matplotlib.pyplot as plt
 
-x_values = [[1, 2], [1.5, 2.5], [2, 3], [5, 6], [8, 9], [5, 5], [6, 9], [1, 1], [1, 4], [1, 8], [1.5, 6], [4, 2], [3, 4], [6, 1], [8, 2], [4, 6], [4, 8], [4, 9], [3, 9], [6.5, 4], [6, 6], [8, 6], [9, 1], [9, 4], [5, 7], [7, 7], [0.5, 9], [1, 7], [8, 3], [8, 1], [0.8, 9]]
+x_values = [[1, 2], [1.5, 2.5], [2, 3], [5, 6], [8, 9],
+            [5, 5], [6, 9], [1, 1], [1, 4], [1, 8],
+            [1.5, 6], [4, 2], [3, 4], [6, 1], [8, 2],
+            [4, 6], [4, 8], [4, 9], [3, 9], [6.5, 4],
+            [6, 6], [8, 6], [9, 1], [9, 4], [5, 7],
+            [7, 7], [0.5, 9], [1, 7], [8, 3], [8, 1],
+            [0.8, 9], [5, 3.5], [6, 2], [4, 5], [3, 7],
+            [3.8, 2.8]]
+
+y_values = [1, 1, 1, 0, 0,
+            0, 0, 1, 1, 1,
+            1, 1, 1, 1, 0,
+            0, 1, 1, 1, 0,
+            0, 0, 1, 0, 0,
+            0, 1, 1, 0, 1,
+            1, 0, 0, 0, 1,
+            0]
+
 x_train = np.array(x_values, dtype=np.float32)
 x_train = x_train.reshape(-1, 2)
-
-y_values = [1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1]
-#y_values = [2 * i + 1 for i in x_values]
 y_train = np.array(y_values, dtype=np.float32)
 y_train = y_train.reshape(-1, 1)
 
 inputDim = 2
-outputDim = 1
-learningRate = 0.3
+learningRate = 0.2
 epochs = 4000
 
 model = MultiLayerPerceptron(inputDim)
@@ -67,10 +80,10 @@ x_train1 = np.array(one_values, dtype=np.float32)
 x_train1 = x_train1.reshape(-1, 2)
 
 plt.clf()
-plt.plot(x_train0[:, 0] * heat_map_resolution / 10, x_train0[:, 1] * heat_map_resolution / 10, '.')#, label='data', alpha=0.5)
-plt.plot(x_train1[:, 0] * heat_map_resolution / 10, x_train1[:, 1] * heat_map_resolution / 10, '*')
+plt.plot(x_train0[:, 0] * heat_map_resolution / 10, x_train0[:, 1] * heat_map_resolution / 10, '.')
+plt.plot(x_train1[:, 0] * heat_map_resolution / 10, x_train1[:, 1] * heat_map_resolution / 10, '.')
 plt.ylim(0, heat_map_resolution)
-plt.imshow(heat_map_data, cmap='BuGn')
+plt.imshow(heat_map_data, cmap='BrBG_r')
 plt.colorbar()
 plt.show()
 
